284acff3c869d48942a775f692e14976a2e4fb7a
seu nome de usuário <renilson_fer@hotmail.com>
2022-12-20 01:57:51 -0300

    rtgfsdfv
    fdgrgerger

--- scriptjs.js
+++ scriptjs.js
@@ -86,7 +86,7 @@ function removerAcentos(str) {
 
 function carrega_listas() {
 
-  document.getElementById("Label5").innerHTML = '-------';
+  document.getElementById("Label5").innerHTML = ' ------- ';
 
   const Url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSrpY-EsBoSiOxGzo1Ffj61ea3QudtLBWbd4oceD-KAJlC-w0BV1YWCDpbPTPGCSX8Hhw3pYQOzUf7m/pub?output=csv';
 
@@ -160,7 +160,7 @@ function analisedefala3(plac3) {
 
         document.getElementById('Label4').innerHTML =pfalada;
 
-        document.getElementById("Label5").innerHTML =  placafalada;
+        document.getElementById('Label5').innerHTML =  placafalada;
         
         falarapp.falartexto(ret);
 
@@ -168,7 +168,7 @@ function analisedefala3(plac3) {
      }else{
 
       document.getElementById('Label4').innerHTML ='fala não reconhecida!';
-      document.getElementById("Label5").innerHTML =  '-------';
+      document.getElementById('Label5').innerHTML =  ' ------- ';
       falarapp.falartexto('não entendi!');
 
      }
